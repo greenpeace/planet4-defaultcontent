4824286872abef7c71fc1366ea297ce93f1f00cb
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2018-10-06 10:38:53 +0300

    Get rid of the double v on the tag

--- update_nro_generator.sh
+++ update_nro_generator.sh
@@ -15,7 +15,7 @@ git clone https://github.com/greenpeace/planet4-nro-generator
 echo ""
 echo "Replacing the previous sql file in the Dockerfile tag with the latest one"
 echo ""
-sed -i "s/planet4-defaultcontent_wordpress-.*.sql/planet4-defaultcontent_wordpress-v${CIRCLE_TAG}.sql/g" ${PROJECT_NRO_GEN_PATH}Dockerfile
+sed -i "s/planet4-defaultcontent_wordpress-.*.sql/planet4-defaultcontent_wordpress-${CIRCLE_TAG}.sql/g" ${PROJECT_NRO_GEN_PATH}Dockerfile
 
 echo ""
 echo "Commiting changes in the repository"
